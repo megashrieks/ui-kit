69b5bbaedaf16baf3a1ae71a779fc0c0ddbe02a2
megashrieks <shrikanthbudya@gmail.com>
2018-12-17 23:55:28 +0530

    Added the info warning and success buttons.

--- src/library/Button/Button.js
+++ src/library/Button/Button.js
@@ -7,6 +7,13 @@ function Button(props) {
 			<button className="cui btn btn-danger">
 				{props.children} danger
 			</button>
+			<button className="cui btn btn-warning">
+				{props.children} warning
+			</button>
+			<button className="cui btn btn-success">
+				{props.children} success
+			</button>
+			<button className="cui btn btn-info">{props.children} info</button>
 		</Fragment>
 	);
 }
